f15fbb3e8f931e433a5805a7e04d69a816dd3d1d
ASD781 <55333948+asdeshpande888@users.noreply.github.com>
2020-04-24 18:49:51 -0400

    Merge branch 'master' of C:\Users\akshi\OneDrive\Desktop\Programs\Kotlin\TodoListApp with conflicts.

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.todolistapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 open class MainActivity : AppCompatActivity() {
+
+    var userId : String? = null;
+    var userData: Map<String, Any>? = null;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val signInActivity = Intent(this, MainActivity::class.java)
+        //startActivityForResult(signInActivity, )
+
+        val db = Firebase.firestore
+        db.collection("Users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("TAG", "${document.id} => ${document.data}")
+                    userId = document.id;
+                    userData = document.data;
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("TAG", "Error getting documents.", exception)
+            }
+
+
+
     }
 }
