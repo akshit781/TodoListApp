8968486bfcd667b2249d11aa221fbdfd23700a23
Adarsh Suresh <adarshs3@illinois.edu>
2020-04-24 17:43:21 -0500

    Created Firebase Auth skeleton

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.todolistapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
